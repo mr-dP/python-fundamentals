e6cf3af011eee82917785024bdc105acd60258fc
dipankar_1993@yahoo.co.in <dipankar_1993@yahoo.co.in>
2023-06-21 15:40:40 +0000

    28. Operator Precedence

---
+++ 028_operator_precedence.py
@@ -0,0 +1,97 @@
+# Databricks notebook source
+100 - 20 + 50
+
+# COMMAND ----------
+
+(100 - 20) + 50
+
+# COMMAND ----------
+
+100 - (20 + 50)
+
+# COMMAND ----------
+
+10 * 4 + 5 * 5
+
+# COMMAND ----------
+
+(10 * 4) + (5 * 5)
+
+# COMMAND ----------
+
+10 * 2 ** 3
+
+# COMMAND ----------
+
+10 * (2 ** 3)
+
+# COMMAND ----------
+
+-10 ** 2
+
+# COMMAND ----------
+
+-(10 ** 2)
+
+# COMMAND ----------
+
+10 ** -2
+
+# COMMAND ----------
+
+10 ** (-2)
+
+# COMMAND ----------
+
+principal = 100
+apr = 0.1
+years = 10
+
+future_value = principal * ((1 + apr / 12) ** (years * 12))
+print(future_value)
+
+# COMMAND ----------
+
+principal = 100
+apr = 0.1
+years = 10
+
+future_value = principal * (1 + apr / 12) ** (years * 12)
+print(future_value)
+
+# COMMAND ----------
+
+2 ** -8
+
+# COMMAND ----------
+
+2 ** (-8)
+# The unary "-" operator have higher precedence when appears to the right of the "**" operator
+
+# COMMAND ----------
+
+-2 ** 0.5
+
+# COMMAND ----------
+
+-(2 ** 0.5)
+
+# COMMAND ----------
+
+(-2) ** 0.5
+
+# COMMAND ----------
+
+n = 10
+
+# COMMAND ----------
+
+n * n - 1
+
+# COMMAND ----------
+
+n * (n - 1)
+
+# COMMAND ----------
+
+
